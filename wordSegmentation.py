bf50ff183fb1f06b7d7eea2754a9977a1f499585
elfphabet621 <elfphabet@gmail.com>
2021-12-05 17:12:30 +0700

    build basic

---
+++ wordSegmentation.py
@@ -0,0 +1,12 @@
+from underthesea import word_tokenize
+with open("./data/evc.txt", encoding='utf-8', mode='r') as fread:
+    with open("./data/wseg.txt", encoding='utf-8', mode='w') as fwrite:
+        while True:
+            eng_line = fread.readline()
+            fwrite.write(eng_line)
+            if eng_line == '':
+                break
+            viet_line = fread.readline()
+            fwrite.write(word_tokenize(viet_line, format="text"))
+            fread.readline()
+            fwrite.write("\n\n")
